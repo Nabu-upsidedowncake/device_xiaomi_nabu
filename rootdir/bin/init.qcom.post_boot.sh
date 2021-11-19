2bb5bff3d5d31fa54c2edb512fee2b46e53a4b77
raghavt20 <raghavtandon2000@gmail.com>
2021-11-19 14:46:16 +0530

    nabu: rootdir: Modify the cpuset setting
    - These values were taken from https://android.googlesource.com/device/google/coral/+/refs/heads/android11-d1-b-release/init.hardware.rc#560
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -4804,12 +4804,12 @@ case "$target" in
 	echo 10 > /proc/sys/kernel/sched_group_downmigrate
 	echo 0 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
-	# cpuset parameters
-	echo 0-2     > /dev/cpuset/background/cpus
-	echo 0-3     > /dev/cpuset/system-background/cpus
-	echo 4-7     > /dev/cpuset/foreground/boost/cpus
-	echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-	echo 0-7     > /dev/cpuset/top-app/cpus
+        # cpuset parameters
+        echo 0-7     > /dev/cpuset/top-app/cpus
+        echo 0-3,5-6 > /dev/cpuset/foreground/cpus
+        echo 0-1     > /dev/cpuset/background/cpus
+        echo 0-3     > /dev/cpuset/system-background/cpus
+        echo 0-3     > /dev/cpuset/restricted/cpus
 
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
