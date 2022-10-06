fd518d02a38a24679a1066f4c48b997f33459daa
kubersharma001 <kubersharma001@gmail.com>
2022-10-06 11:11:23 +0000

    nabu: Remove some packages
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+   Dialer \
+   GoogleDialer \
+   com.google.android.dialer.support \
+   com.android.phone.common
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
