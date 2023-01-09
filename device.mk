dccbe13de5d78ef7578c05efa2d121b79040a958
johnmart19 <johnivan19999@gmail.com>
2023-01-09 21:23:53 +0200

    nabu: Audio: Enable audio adsprpcd daemon
    Enable compilation of audio adsprpcd daemon.
    
    Small info: This is part of VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio@04baa1217e4ab263d69cc6af8e06c0ee7be456ee
    
    - We have "libadsp_default_listener.so" library and other dependant hals for this environment
    - Sepolicy for this we are having by default PixelExperience/device_qcom_sepolicy_vndr@03962670ef0ab32312bdc7346f146d2ac5e8017b
    
    Test:
    
    Targets compiled:
    * vendor/bin/audioadsprpcd
    * vendor/etc/init/vendor.qti.audio-adsprpc-service.rc
    
    Device booted fine to system, audio is working as usual.
    
    Reference of this commit: VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio@fb56879b933c0a1668c0c1d0beacf801cba6a511
    
    Co-authored-by: George Gao <georgao@codeaurora.org>

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Audio Daemon
+PRODUCT_PACKAGES += audioadsprpcd
+PRODUCT_PACKAGES += vendor.qti.audio-adsprpc-service.rc
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
