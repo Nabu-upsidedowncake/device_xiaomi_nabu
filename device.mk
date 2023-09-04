f04e068e7ba5642fc304ef2821b6c04deab97af8
Harshit Jain <reach@harsh1998.dev>
2023-09-04 00:01:38 +0000

    nabu: kang all bluetooth stuff from crdroid 8150
    * keep all properties in vendor.prop instead of system ext, product and vendor
    * hopefully this fixes offload
    
    Signed-off-by: Harshit Jain <reach@harsh1998.dev>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -170,8 +170,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
