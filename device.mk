d0485db46eb9cb625eee6975dc2e1f6d619f634c
Chenyang Zhong <zhongcy95@gmail.com>
2023-09-04 20:51:50 +0000

    nabu: ship cgroups.json and task_profiles.json in vendor
    Google's prebuilt cgroups.json and task_profiles.json for
    products launched with previous API levels only covers
    ro.product.first_api_level >= 28. [1]
    
    Test: boot and check /dev/stune/
    
    [1] system/core/libprocessgroup/profiles/
    
    Change-Id: I89579a4f906640992c1ec9ae603db64fac184011
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -404,6 +404,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-nabu \
     android.hardware.power-impl
 
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
