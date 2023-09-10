d7a657907a4ada77a5e4b683c50fdf5b3862773f
madmax7896 <madmax78968@gmail.com>
2023-09-10 10:16:28 -0300

    nabu: Import Dolby audio blobs
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -318,7 +318,11 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libOmxVidcCommon
+    libOmxVidcCommon \
+    libstagefrighthw \
+    libavservices_minijail_vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
