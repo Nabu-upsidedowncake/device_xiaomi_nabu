fb93ffb9d0de1bc98e4922dc0ac15f1d30e7bc8f
saikiran2001 <bjsaikiran@gmail.com>
2023-09-13 16:11:04 +0200

    nabu: Switch to clang 17.0.4
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,14 +106,12 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu
 TARGET_KERNEL_CONFIG := nabu_defconfig
-TARGET_KERNEL_CLANG_COMPILE := false
-TARGET_KERNEL_NEW_GCC_COMPILE := true
-KERNEL_TOOLCHAIN_PREFIX_arm64 := aarch64-elf-
-KERNEL_TOOLCHAIN_PREFIX_arm := arm-eabi-
-KERNEL_TOOLCHAIN_arm64 := $(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin
-KERNEL_TOOLCHAIN_arm := $(PWD)/prebuilts/gcc/linux-x86/aarch64/arm-eabi/bin
-KERNEL_LD := LD=$(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin/aarch64-elf-ld.lld
-KERNEL_MAKE_FLAGS += LD=ld.lld CC=aarch64-elf-gcc HOSTCXX=aarch64-elf-g++ HOSTCC=gcc
+TARGET_KERNEL_CLANG_VERSION := r498229b
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r498229b/bin
+KERNEL_SUPPORTS_LLVM_TOOLS := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
