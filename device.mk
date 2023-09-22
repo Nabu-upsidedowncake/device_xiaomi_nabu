d50634dd3bae3bc3fb265668cd5c81693bf4def7
Kazuki H <kazukih0205@gmail.com>
2023-09-22 20:34:58 +0900

    nabu: Use configs from API 30
    That's what our vendor uses.
    
    Change-Id: I8960671b2cf24e1d0c61bf6acb4839c42130c275
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -405,8 +405,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-impl
 
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Public libraries
 PRODUCT_COPY_FILES += \
